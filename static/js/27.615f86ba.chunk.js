7342c3e684ad0c8765c57bc799c803aad7da88ea
artem <56235052+grafgooseman@users.noreply.github.com>
2022-08-10 06:22:42 -0400

    Updates

---
+++ static/js/27.615f86ba.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclient-app-artem-version"]=this["webpackJsonpclient-app-artem-version"]||[]).push([[27],{601:function(p,e){}}]);
+//# sourceMappingURL=27.615f86ba.chunk.js.map
---
+++ static/js/28.6204a4ba.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclient-app-artem-version"]=this["webpackJsonpclient-app-artem-version"]||[]).push([[28],{651:function(p,e){}}]);
+//# sourceMappingURL=28.6204a4ba.chunk.js.map
